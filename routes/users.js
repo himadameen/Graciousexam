6844a72edc42b320e53a32e9db781499b234c1a4
himmad ameen <ameenhh1620@gmail.com>
2022-08-05 22:37:41 +0530

    my changes

--- routes/users.js
+++ routes/users.js
@@ -3,29 +3,53 @@ const router = express.Router();
 const db = require('../db');
 
 
-router.get('/allusers', async (req, res) =>{
-    try{
-        const response = await db.promise().query("SELECT * FROM data_base2");
+router.get('/allusers', async (req, res) => {
+    try {
+        const response = await db.promise().query("SELECT * FROM data_base");
         console.log(response[0]);
         res.send(response[0]);
     }
-    catch(err){
+    catch (err) {
         res.status(400).json(err);
     }
 })
 
-router.get('/:user_id', async(req, res) => {
-    try{
-        // const response = await db.promise().query("SELECT * FROM data_base2 WHERE userid = ");
+router.get('/:user_id', async (req, res) => {
+    try {
         const us_id = req.params.user_id;
-        const response = await db.promise().query(`SELECT * FROM data_base2 WHERE userid= '${us_id}'`);
+        const response = await db.promise().query(`SELECT * FROM data_base WHERE userid= '${us_id}'`);
         console.log(response[0]);
         res.send(response[0]);
     }
-    catch(err){
+    catch (err) {
         res.status(400).json(err);
     }
 })
+
+
+router.post("/get_user", async (req, res) => {
+
+    try {
+        const user = req.body.name;
+        const response = await db.promise().query(`SELECT  *  FROM users WHERE name = '${user}'`);
+        res.status(200).json(response[0]);
+    }
+    catch (err) {
+        res.status(400).json(err);
+    }
+});
+
+
+router.delete("/delete_user", async (req, res) => {
+
+    try {
+        const user = req.body.name;
+        const response = await db.promise().query(`DELETE  FROM users WHERE username = '${user}'`);
+        res.status(200).json(response[0]);
+    } catch (err) {
+        res.status(400).json(err);
+    }
+});
 
 
 module.exports = router;
